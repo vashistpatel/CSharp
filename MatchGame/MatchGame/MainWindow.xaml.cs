fe858a53cfd2240f18fa0d76eee63a17f59b5f5e
vashistpatel <55363649+vashistpatel@users.noreply.github.com>
2024-01-06 16:44:57 -0500

    Added grid and emoji in them

---
+++ MatchGame/MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupGame();
+        }
+
+        private void SetupGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+               "🐙","🐙",
+               "🐵","🐵",
+               "🐶","🐶",
+               "🐮","🐮",
+               "🐯","🐯",
+               "🦓","🦓",
+               "🐔","🐔",
+               "🐨","🐨",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                System.Diagnostics.Debug.WriteLine(index);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
